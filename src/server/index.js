39a7e0fa983f03576eaf2b4392dbbdd6b3f3a567
debalina <mukherjee.debalina@gmail.com>
2020-05-13 04:55:22 -0700

    feat(get-curriculum): remove hardcoded cohort Id to get data for the selected cohort

--- src/server/index.js
+++ src/server/index.js
@@ -2,22 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const api = require('./utils/api');
 const curriculum = require('./routes/curriculum');
-
+const cohortMap = require('../../config/cohortMap');
 
 const port = process.env.PORT || 9001;
 
 const app = express();
 app.use(cors());
 
-app.get("/curriculum/:cohortId", async (req, res, next) => {
-  const data = await api.getCurriculum(req.params.cohortId);
-  res.status(200).send(data)
+app.get('/curriculum/:cohortName', async (req, res) => {
+  const cohortId = cohortMap[req.params.cohortName];
+  const data = await api.getCurriculum(cohortId);
+  res.status(200).send(data);
 });
-
 
 app.use('/scheduler/curriculum', curriculum);
 
 app.listen(port, () => {
  console.log(`Server running on port ${port}`);
 });
-
