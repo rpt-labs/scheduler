802c95f70982225841368312048a8d54608cbce2
debalina <mukherjee.debalina@gmail.com>
2020-05-25 23:32:53 -0700

    feat(summary): update icons, add background color

--- src/client/components/CohortSummary/SummaryContent/SummaryContent.js
+++ src/client/components/CohortSummary/SummaryContent/SummaryContent.js
@@ -11,17 +11,17 @@ const SummaryContent = (props) => {
   const sectionDetails = sections ? sections.map((section) => ({ title: section.title, units: section.units })) : [];
   const sortedSections = sectionDetails.sort((a, b) => sectionsList.indexOf(a.title) - sectionsList.indexOf(b.title));
 
-  const getUnitTitle = (unit) => (
-    <Segment.Group>
+  const getUnitTitle = (unit, backgroundColor) => (
+    <Segment.Group style={{ color: 'grey' }}>
       {unit.content_files.map((file) =>
         file.visible ? (
-          <Segment key={file.title}>
-            <Icon name="hide" style={{ cursor: 'pointer', marginRight: '16px', color: 'grey' }} />
-            <span style={{ color: '#006600' }}>{file.title}</span>
+          <Segment key={file.title} style={{ backgroundColor: `${backgroundColor}` }}>
+            <Icon name="checkmark" style={{ cursor: 'pointer', marginRight: '16px', color: '#006600' }} />
+            <span>{file.title}</span>
           </Segment>
         ) : (
           <Segment key={file.title}>
-            <Icon name="unhide" style={{ cursor: 'pointer', marginRight: '16px', color: '#006600' }} />
+            <Icon name="x" style={{ cursor: 'pointer', marginRight: '16px', color: '#bf0000' }} />
             <span style={{ color: 'grey' }}>{file.title}</span>
           </Segment>
         )
@@ -29,12 +29,12 @@ const SummaryContent = (props) => {
     </Segment.Group>
   );
 
-  const getUnitContent = (unit, iconName, iconColor, textColor) => (
+  const getUnitContent = (unit, iconName, color, backgroundColor) => (
     <>
-      <div key={unit.title}>
-        <Icon name={iconName} style={{ cursor: 'pointer', marginRight: '8px', color: `${iconColor}` }} />
-        <span style={{ color: `${textColor}`, fontSize: '20px' }}>{unit.title}</span>
-        {getUnitTitle(unit)}
+      <div key={unit.title} style={{ color: `${color}` }}>
+        <Icon name={iconName} style={{ cursor: 'pointer', marginRight: '8px' }} />
+        <span style={{ fontSize: '20px' }}>{unit.title}</span>
+        {getUnitTitle(unit, backgroundColor)}
       </div>
       <Divider section />
     </>
@@ -54,10 +54,10 @@ const SummaryContent = (props) => {
             <Segment>
               {section.units.map((unit) =>
                 unit.visible && unit.content_files.every((file) => file.visible === true)
-                  ? getUnitContent(unit, 'hide', 'grey', '#006600')
+                  ? getUnitContent(unit, 'checkmark', '#006600', '#F0FFF3')
                   : unit.visible && unit.content_files.some((file) => file.visible === true)
-                  ? getUnitContent(unit, 'hide', 'grey', '#FDAF08')
-                  : getUnitContent(unit, 'unhide', '#006600', 'grey')
+                  ? getUnitContent(unit, 'checkmark', '#FDAF08', '#F0FFF3')
+                  : getUnitContent(unit, 'x', '#BF0000')
               )}
             </Segment>
           </Segment.Group>
